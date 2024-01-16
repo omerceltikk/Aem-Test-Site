0cb79330990c66b32b580122130f502eecd80638
Ömer Faruk ÇELTİK <omerfceltik@gmail.com>
2024-01-16 13:54:44 +0300

    update

--- blocks/certified/certified.js
+++ blocks/certified/certified.js
@@ -7,8 +7,8 @@ export default function decorate(block) {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'certifiedCards-card-image';
-      else div.className = 'certifiedCards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'certified-card-image';
+      else div.className = 'certified-card-body';
     });
     ul.append(li);
   });
